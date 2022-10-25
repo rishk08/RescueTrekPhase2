59df4e9a36888c0d44ab2e2e9daac4d1942d5495
Joey <71848224+josephgleal@users.noreply.github.com>
2022-10-25 11:42:22 -0500

    added priority dictionary to IPCamera class

--- Feed.py
+++ Feed.py
@@ -1,17 +1,16 @@
 from pkgutil import get_data
 import histogram as w
-import cv2
 from threading import Thread
 from abc import ABC, abstractmethod
 # from Model import GunDetection
-from collections import deque
+
 
     
 class Feed(ABC):
-    def __init__(self, models = None) -> None:
-        super().__init__()
-        #array of models
-        self.models = models
+    # def __init__(self, models = None) -> None:
+    #     super().__init__()
+    #     #array of models
+    #     self.models = models
 
     @abstractmethod
     def __init__(self, model = None):
@@ -51,75 +50,5 @@ class Feed(ABC):
         pass
 
 
-class IPCamera(Feed):
-    def __init__(self, ip, username = "", password = "", models = None):
-        super().__init__(models)
-        self.cap = None
-        self.frame = None
-        self.ip = ip
-        self.deque = deque(maxlen = 20)
 
-        #not secure 
-        self.username = username
-        self.password = password
-
-        #maybe more effecient way to do this
-        self.link = ""
-        
-    #starts the camera stream and initialized the self.cap variable
-    def initialize(self):
-        try:
-            cam_string = "rtsp://"+self.username+":"+self.password+"@"+self.ip+"/1"
-            self.link = cam_string
-            self.cap = cv2.VideoCapture(0)
-        except Exception as Argument:
-            print("error with camera with ip", self.ip, "\n", "Error:", Argument)
-    
-    # attempts to connect to camera feed. Returns True if succesful
-    def verify_feed(self, source):
-        ret, cap = cv2.VideoCapture(source)  
-        if not cap.isOpened():
-            return False
-        else:
-            cap.release()
-        return True
-
-    #Attempt to Re-establish connection with the feed
-    def reconnect(self, source):
-        cap = cv2.VideoCapture(source)  
-        if not cap.isOpened():
-            return False
-        else:
-            self.cap = cap
-        return True
-
-    #Releases camera capture
-    def close(self):
-        self.cap.release()
-
-    #Retrieves an individual frame
-    def get_data(self):
-        ret, frame = self.cap.read()
-        frame = cv2.resize(frame, (400, 400))
-        return frame
-    
-    # gets new frame from camera and sets it as attribute
-    def update_frame(self):
-        frame = get_data()
-        self.frame = frame
-
-    # Runs model to detect threat
-    # Might need to be in the model class
-    def run_model(self, source):
-        self.model.assess(self.frame)
-        # self.model.assess(get_data())
-
-    #primative solution to run all models
-    def run_models(self):
-        for model in self.models:
-            self.run_model(model)
-        
-    #Returns sensor type
-    def get_sensor_type(self):
-        return "camera"
 
