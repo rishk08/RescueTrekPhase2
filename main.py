48af928e75c599657f8970300904432613f9d328
brianjchong <bc3247@tamu.edu>
2022-11-17 23:00:42 -0600

    added classes used in Gui. updated files

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 from system import System
+# from GUI import GUI  
+from GUI import GUI  
+from InputFeedClasses.IPCamera import IPCamera
+# G = GUI(System('config.json'))
+# obj = System('config.json')
+
+# obj.StartUp()
+
+# #Determine way to halt main thread and listen for event to shutdown system
+# while(True):
+#     #Running system
+#     i = 0
+#     if(i == 1):
+#         break;
     
-obj = System('config.json')
+# obj.Shutdown()
+camera = IPCamera("0","admin","admin")
+camera1 = IPCamera("1", "admin", "admin")
+camera2 = IPCamera("1","admin","admin" )# USB Camera
+camera3 = IPCamera("10.224.242.127","admin","Camera123")
+camera4 = IPCamera("10.224.8.239","admin","Camera123")
+# camera5 = IPCamera("10.224.8.239","admin","Camera123")
 
-obj.StartUp()
 
-#Determine way to halt main thread and listen for event to shutdown system
-while(True):
-    #Running system
-    i = 0
-    if(i == 1):
-        break;
-    
-obj.Shutdown()
+# G = GUI([camera,camera,camera2,camera2])
+# G = GUI([camera, camera2])
+G = GUI([camera, camera1, camera3 ])
+G.run()
+# camera3.initialize()
+# camera3.show()
 
+
+#intiialize GUI with no input parameters
