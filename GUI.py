f869dbd568645cedbf34f1a83c019be7f2f7b8c8
Priyanshu Barnwal <70822342+Preebie@users.noreply.github.com>
2023-04-04 13:08:06 -0500

    Added GUI (It works now kinda)

--- GUI.py
+++ GUI.py
@@ -1,4 +1,22 @@
-import Yolov5.detect
-import FaceDetection_v1.detect
+import Yolov5.detect as Yd
 import threading
+import streamlit as st
 
+def intro():
+
+    st.write("# Welcome to Streamlit! 👋")
+    st.sidebar.success("Select a demo above.")
+
+
+def Yolo_site():
+    st.title('RescueTrek')
+    opts = Yd.parse_opt()
+    Yd.main(opts)
+
+if __name__ == "__main__":
+    page_names_to_funcs = {
+        "Gun Detection": Yolo_site,
+    }
+
+    demo_name = st.sidebar.selectbox("Choose a demo", page_names_to_funcs.keys())
+    page_names_to_funcs[demo_name]()    
