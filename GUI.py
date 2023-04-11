a06a95a37b1ccf8aa97fab59b61bf2ec552272c3
rishiamarnath1 <rishiamarnath1@gmail.com>
2023-04-10 23:34:48 -0500

    created navigation bar, added title, added file upload for face detection

--- GUI.py
+++ GUI.py
@@ -3,16 +3,15 @@ import threading
 import streamlit as st
 import signal
 
+import os
+import subprocess
+
 
 def Yolo_site():
-    st.title('RescueTrek')
-
+    # st.title("RescueTrek")
     opts = Yd.parse_opt()
     opts.source = "cam_locations.streams"
     Yd.main(opts)
-    
-
-
 
 
 def write_to_file(filename, stringer):
@@ -21,31 +20,64 @@ def write_to_file(filename, stringer):
     filer.close()
 
 
-def main():
-    num_cams = 0
-    # with open("cam_locations.streams", "r+") as my_file:
-    #     my_file.seek(0)
-    #     my_file.truncate()
-    #     print("CLEARED")
+def facial_detection_page():
+    st.write("Welcome to the Facial Detection Page!")
+
+    # Accept text input for the name
+    name = st.text_input("Enter a name:")
+
+    # Choose a file to upload
+    uploaded_file = st.file_uploader("Choose a file to upload")
+
+    if uploaded_file is not None:
+        # Create a new directory with the given name
+        base_path = "./FaceDetection_v1/facepics"
+        new_folder_path = os.path.join(base_path, name)
+
+        if not os.path.exists(new_folder_path):
+            os.makedirs(new_folder_path)
+
+        # Save the file in the created directory
+        file_path = os.path.join(new_folder_path, f"{name}.jpg")
+
+        with open(file_path, "wb") as f:
+            f.write(uploaded_file.getbuffer())
+
+        st.write(f"You uploaded a file, saved as {name}/{name}.jpg")
+
+
+def home_page():
     done = False
-    camera_location = st.text_input('Add Camera')
+    camera_location = st.text_input("Add Camera")
     emp = st.empty()
     done = emp.button("Done adding cameras!")
 
     if camera_location and not done:
         write_to_file("cam_locations.streams", camera_location + "\n")
         camera_location = ""
-    
+
     if done:
         emp.empty()
         Yolo_site()
 
-if __name__ == '__main__':
+
+def main():
+    num_cams = 0
+
+    st.sidebar.image("rescuetrek.png", use_column_width=True)
+    pages = {"Home": home_page, "Facial Detection": facial_detection_page}
+    page = st.sidebar.radio("Go to", list(pages.keys()))
+
+    # Display the selected page
+    pages[page]()
+    # st.image("rescuetrek.png")
+    # st.markdown("<br>", unsafe_allow_html=True)
+    # st.sidebar.markdown("<br>", unsafe_allow_html=True)
+    # with open("cam_locations.streams", "r+") as my_file:
+    #     my_file.seek(0)
+    #     my_file.truncate()
+    #     print("CLEARED")
+
+
+if __name__ == "__main__":
     main()
-
-
-
-            
-
-
-    
